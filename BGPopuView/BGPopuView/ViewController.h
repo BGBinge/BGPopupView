6b4ca2d034b91b0a37e45f0d643269a478e03a1c
Binge <binge@ranbindemacbook-pro.local>
2018-10-03 23:03:01 +0800

    添加项目

---
+++ BGPopuView/BGPopuView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BGPopuView
+//
+//  Created by 冉彬 on 2018/10/3.
+//  Copyright © 2018年 Binge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BGPopuView/BGPopuView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BGPopuView
+//
+//  Created by 冉彬 on 2018/10/3.
+//  Copyright © 2018年 Binge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
